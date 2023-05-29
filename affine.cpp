02ceba993ff8ca88c57d2bc8ffdc716bc635a073
KXAND <71508678+KXAND@users.noreply.github.com>
2023-05-30 01:26:49 +0800

    修改affine的注释

--- affine.cpp
+++ affine.cpp
@@ -20,23 +20,10 @@ int affine::exgcd(int a, int b, int &x, int &y)
         y = 0;
         return a;
     }
-    int gcd = exgcd(b, a % b, x, y);
-    int temp = x;
-    x = y;
-    y = temp - (a / b) * y;
-    return gcd;
-    // if (b == 0)
-    // {
-    //     x = 1;
-    //     y = 0;
-    //     return a;
-    // }
-    // int ret = exgcd(b, a % b, x, y);
-    // // 忘了就现推
-    // int tmp = y;
-    // y = x - (a / b) * y;
-    // x = tmp;
-    // return ret;
+    int ret = exgcd(b, a % b, y, x);
+    // 忘了就现推
+    y -= (a / b) * x;
+    return ret;
 }
 // ax+by=1中x即a在模b下的逆元
 // 对逆元有n/a = n*x%b
@@ -47,22 +34,21 @@ int affine::getInv(int a, int b)
     return d == 1 ? (x + b) % b : -1;
 }
 
+// 根据密钥生成密文对照表，需要注意的是这里变量名为key，而加解密实际使用的是顺序的真字母表alphabet
 void affine::generateAlphabet(int a, int b)
 {
     int len = alphabet.length();
     for (int i = 0; i < len; i++)
     {
-        // key.push_back((a * i + b) % CHAR_SIZE);
         key.push_back(alphabet[(a * i + b) % CHAR_SIZE]);
     }
-    // alphabet = alphabet.substr(CHAR_SIZE);
     ShowAlphabet(key);
     key.clear();
     for (int i = 0; i < len; i++)
     {
-        // key.push_back((a * i + b) % CHAR_SIZE);
-        key.push_back(alphabet[mInv * ((i - b+CHAR_SIZE)) % CHAR_SIZE]);
+        key.push_back(alphabet[mInv * ((i - b + CHAR_SIZE)) % CHAR_SIZE]);
     }
+    printf("解密表：（密文为明文）\n");
     ShowAlphabet(key);
 }
 
